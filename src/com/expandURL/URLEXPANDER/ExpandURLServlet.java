3ce27239a356b0a920df28f4dfea831d6a3f09f4
romil <romil@ubuntu.ubuntu-domain>
2013-05-05 15:16:58 +0530

    Added callback feature

--- src/com/expandURL/URLEXPANDER/ExpandURLServlet.java
+++ src/com/expandURL/URLEXPANDER/ExpandURLServlet.java
@@ -1,120 +1,108 @@
-/*
- * Author : Romil Bansal
- * Example : http://www.urltoshorturl.appspot.com/expand?url=http://bit.ly/Lfj4kk 
- */
-package com.expandURL.URLEXPANDER;
+package com.expandURL.URLExpander;
 
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
-import net.sf.json.JSONObject;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
-/**
- * Servlet implementation class ExpandURLServlet
- */
-@WebServlet("/ExpandURLServlet")
+@SuppressWarnings("serial")
 public class ExpandURLServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ExpandURLServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
 		resp.setContentType("text/plain");
-		String ur="";
+		String shortURL="";
+		String callback="";
 		URL url=null;
+		boolean flag = true;
 		try{
-			ur=req.getParameter("url");
-			url=new URL(ur);
-		try{
-			URLConnection conn=url.openConnection();
-			conn.getHeaderFields();
-			//System.out.println(conn.getURL().toString().replaceAll("/", "\\\\/"));
-			JSONObject json=new JSONObject();
-			json.put("end_url", conn.getURL().toString().replaceAll("/", "\\/"));
-			json.put("start_url", url.toString().replaceAll("/", "\\/"));
-			json.put("status", "OK");
-			if(conn.getURL().toString().trim().equals(url.toString().trim()))
-				json.put("redirects", "0");
-			else
-				json.put("redirects", "1");
-			resp.getWriter().println(json.toString());
-		}
-		catch (Exception e) {
-
-			// TODO: handle exception
+			shortURL=req.getParameter("url");
+			url=new URL(shortURL);
+			callback=req.getParameter("callback");
+			if(callback==null||callback.equals("null"))
+				flag=false;
+			//the first attempt (of two attempts) to expand the URL making request to site
+			//	
 			try{
 				URLConnection conn=url.openConnection();
 				conn.getHeaderFields();
-				//System.out.println(conn.getURL().toString().replaceAll("/", "\\\\/"));
-				JSONObject json1=new JSONObject();
-				json1.put("end_url", conn.getURL().toString().replaceAll("/", "\\/"));
-				json1.put("start_url", url.toString().replaceAll("/", "\\/"));
-				json1.put("status", "OK");
+				JSONObject json=new JSONObject();
+				json.put("end_url", conn.getURL().toString().replaceAll("/", "\\/"));
+				json.put("start_url", url.toString().replaceAll("/", "\\/"));
+				json.put("status", "OK");
 				if(conn.getURL().toString().trim().equals(url.toString().trim()))
-					json1.put("redirects", "0");
+					json.put("redirects", "0");
 				else
-					json1.put("redirects", "1");
-				//System.out.println("iuouo");
-				resp.getWriter().println(json1.toString());
+					json.put("redirects", "1");
+				if(flag)
+					resp.getWriter().println(callback+"("+json.toString()+")");
+				else
+					resp.getWriter().println(json.toString());
 			}
-			catch (Exception ef) {
-				try {
-					JSONObject json2=new JSONObject();
-					json2.put("start_url", url.toString().replaceAll("/", "\\/"));
-					json2.put("end_url", url.toString().replaceAll("/", "\\/"));
-					json2.put("redirects", "0");
-					json2.put("status", "Invalid");
-					resp.getWriter().println(json2.toString());
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+			catch (Exception e) {
+
+				// The first attempts fails//
+				//The second attempt (of two attempts) to expand the URL
+				try{
+					URLConnection conn=url.openConnection();
+					conn.getHeaderFields();
+					//System.out.println(conn.getURL().toString().replaceAll("/", "\\\\/"));
+					JSONObject json1=new JSONObject();
+					json1.put("end_url", conn.getURL().toString().replaceAll("/", "\\/"));
+					json1.put("start_url", url.toString().replaceAll("/", "\\/"));
+					json1.put("status", "OK");
+					if(conn.getURL().toString().trim().equals(url.toString().trim()))
+						json1.put("redirects", "0");
+					else
+						json1.put("redirects", "1");
+					if(flag)
+						resp.getWriter().println(callback+"("+json1.toString()+")");
+					else
+						resp.getWriter().println(json1.toString());
 				}
 
-				// TODO: handle exception
+				//Failed to expand the page
+				//return default
+				catch (Exception ef) {
+					try {
+						JSONObject json2=new JSONObject();
+						json2.put("start_url", url.toString().replaceAll("/", "\\/"));
+						json2.put("end_url", url.toString().replaceAll("/", "\\/"));
+						json2.put("redirects", "0");
+						json2.put("status", "Invalid");
+						if(flag)
+							resp.getWriter().println(callback+"("+json2.toString()+")");
+						else
+							resp.getWriter().println(json2.toString());
+					} catch (JSONException e1) {
 
+					}
+				}
 			}
 		}
-		}
+		//Problem with code
+		// return default
 		catch (Exception e) {
 			JSONObject json2=new JSONObject();
 			try {
-				if(ur==null)
-					ur="";
-				json2.put("start_url", ur.toString().replaceAll("/", "\\/"));
-				json2.put("end_url", ur.toString().replaceAll("/", "\\/"));
+				if(shortURL==null)
+					shortURL="";
+				json2.put("start_url", shortURL.toString().replaceAll("/", "\\/"));
+				json2.put("end_url", shortURL.toString().replaceAll("/", "\\/"));
 				json2.put("redirects", "0");
 				json2.put("status", "Invalid");
-				resp.getWriter().println(json2.toString());
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				if(flag)
+					resp.getWriter().println(callback+"("+json2.toString()+")");
+				else
+					resp.getWriter().println(json2.toString());
+			} catch (JSONException e1) {
+
 			}
-			// TODO: handle exception
 		}
+		//System.out.println(json.toString());
+		//resp.getWriter().println("{ end_url:"+conn.getURL().toString().replaceAll("\\", "")+"}");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
 }
